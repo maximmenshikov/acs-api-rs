7597cf585a6fb7a33d0f3716a283240348de0b9b
Maxim Menshikov <maxim@menshikov.org>
2024-11-04 20:40:22 +0000

    Add SetParameterValues

---
+++ src/request/set_parameter_values.rs
@@ -0,0 +1,21 @@
+use crate::parameter_value::ParameterValue;
+use serde::{Deserialize, Serialize};
+use crate::util::accessor::*;
+
+#[derive(Serialize, Deserialize, PartialEq, Clone, Debug)]
+#[repr(C)]
+pub struct SetParameterValues {
+    #[serde(default = "unset_str")]
+    pub name: String,
+    #[serde(default = "unset_vec_parameter_value")]
+    pub parameter_values: Vec<ParameterValue>,
+}
+
+impl SetParameterValues {
+    pub fn new(parameter_values: Vec<ParameterValue>) -> Self {
+        return SetParameterValues {
+            name: "setParameterValues".to_string(),
+            parameter_values: parameter_values.clone(),
+        };
+    }
+}
