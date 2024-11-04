644679480845b5ba483d2c62965fe9ce989f4a58
Maxim Menshikov <maxim@menshikov.org>
2024-11-04 20:40:41 +0000

    Add task enqueuer

--- src/connection.rs
+++ src/connection.rs
@@ -1,5 +1,8 @@
+use crate::request::set_parameter_values::*;
 use reqwest::blocking::Client;
 use crate::device::*;
+use crate::parameter_value::*;
+
 pub struct AcsConnection {
     pub addr: String,
 }
@@ -25,8 +28,27 @@ impl AcsConnection {
             let val: Vec<AcsDevice> = serde_json::from_str(&s)?;
             return Ok(val);
         } else {
-            println!("Failed to send request: {}", response.status());
-            return Ok(Vec::new());
+            return Err(Box::from(format!("Response indicates failure: {}", response.status())));
+        }
+    }
+
+    pub fn enqueue_task(&self, _device_id: String, parameter_values: Vec<ParameterValue>) -> Result<(), Box<dyn std::error::Error>> {
+        let client = Client::new();
+
+        // Define the URL
+        let url = self.addr.clone() + "/devices";
+
+        let req = SetParameterValues::new(parameter_values.clone());
+        // Send a GET request
+        let response = client
+            .post(&url)
+            .json(&req)  // Automatically serializes the struct to JSON
+        .send()?;
+
+        if response.status().is_success() {
+            return Ok(());
+        } else {
+            return Err(Box::from(format!("Response indicates failure: {}", response.status())));
         }
     }
 }
