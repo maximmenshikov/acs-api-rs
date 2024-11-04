d5d0d6fcc2a432bf8f26c7f65fe5e64f842b0556
Maxim Menshikov <maxim@menshikov.org>
2024-11-04 20:17:57 +0000

    Update Device data

--- src/device.rs
+++ src/device.rs
@@ -2,8 +2,25 @@ use serde::{Deserialize, Serialize};
 use crate::util::accessor::*;
 
 #[derive(Serialize, Deserialize, PartialEq, Clone, Debug)]
+#[serde(rename_all = "camelCase")]
 #[repr(C)]
 pub struct AcsDevice {
     #[serde(default = "unset_str")]
     pub id: String,
+    #[serde(default = "unset_str")]
+    pub serial_number: String,
+    #[serde(default = "unset_str")]
+    pub manufacturer: String,
+    #[serde(default = "unset_str")]
+    pub model: String,
+    #[serde(default = "unset_str")]
+    pub firmware_version: String,
+    #[serde(default = "unset_str")]
+    pub last_online: String,
+    #[serde(default = "unset_str")]
+    pub created_at: String,
+    #[serde(default = "unset_str")]
+    pub updated_at: String,
+    #[serde(default = "unset_str")]
+    pub status: String,
 }
