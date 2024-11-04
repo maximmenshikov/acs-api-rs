1986ceb2c4e095f8e4e78bf550512d06fd67f712
Maxim Menshikov <maxim@menshikov.org>
2024-11-04 20:40:30 +0000

    Add more accessors

--- src/util/accessor.rs
+++ src/util/accessor.rs
@@ -1,3 +1,13 @@
+use crate::parameter_value::*;
+
 pub fn unset_str() -> String {
     return "".to_string();
+}
+
+pub fn unset_parameter_value() -> ParameterValue {
+    return ParameterValue::new("", "", "");
+}
+
+pub fn unset_vec_parameter_value() -> Vec<ParameterValue> {
+    return Vec::new();
 }
