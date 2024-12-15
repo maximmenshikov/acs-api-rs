77e1d0381460554eb8c0019cd843466e0739aad6
Maxim Menshikov <maxim@menshikov.org>
2024-12-15 10:05:29 +0000

    Support custom serializer

--- src/parameter_value.rs
+++ src/parameter_value.rs
@@ -1,7 +1,9 @@
+use serde_json::Value;
+use serde::Serializer;
 use crate::util::accessor::*;
 use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize, PartialEq, Clone, Debug)]
+#[derive(Deserialize, PartialEq, Clone, Debug)]
 #[repr(C)]
 pub struct ParameterValue {
     #[serde(default = "unset_str")]
@@ -27,5 +29,39 @@ impl ParameterValue {
             self.value.clone(),
             self.value_type.clone(),
         ];
+    }
+}
+
+impl Serialize for ParameterValue {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        match self.value_type.as_str() {
+            "xsd:string" => {
+                let output = vec![
+                    Value::String(self.parameter.clone()),
+                    Value::String(self.value.clone())];
+                output.serialize(serializer)
+            }
+            "xsd:boolean" => {
+                let parsed_value = self.value.parse::<bool>().unwrap_or(false);
+                let output = vec![
+                    Value::String(self.parameter.clone()),
+                    Value::Bool(parsed_value)];
+                output.serialize(serializer)
+            }
+            "xsd:unsignedInt" => {
+                let parsed_value = self.value.parse::<u32>().unwrap_or(0);
+                let output = vec![
+                    Value::String(self.parameter.clone()),
+                    Value::Number(parsed_value.into())];
+                output.serialize(serializer)
+            }
+            _ => {
+                let output = vec![self.parameter.clone(), self.value.clone()];
+                output.serialize(serializer)
+            }
+        }
     }
 }
