b9c6d341c351fdbf4bb65caf58903e2f97eaadd4
Maxim Menshikov <maxim@menshikov.org>
2025-03-02 13:05:24 +0000

    Allow uploading files to GenieACS

--- src/connection.rs
+++ src/connection.rs
@@ -7,6 +7,7 @@ use crate::request::refresh_object::*;
 use crate::request::set_parameter_values::*;
 use crate::request::simple_command::*;
 use reqwest::blocking::Client;
+use reqwest::header::{HeaderMap, HeaderValue};
 use serde_json::Value;
 use std::collections::HashMap;
 
@@ -320,6 +321,49 @@ impl AcsConnection {
         } else {
             client.delete(&url).send()?
         };
+
+        if response.status().is_success() {
+            return Ok(());
+        } else {
+            return Err(Box::from(format!(
+                "Response indicates failure: {}",
+                response.status()
+            )));
+        }
+    }
+
+    pub fn upload_file(&self,
+        name: &str,
+        path: &str,
+        file_type: &str,
+        oui: &str,
+        product_class: &str,
+        version: &str) -> Result<(), Box<dyn std::error::Error>> {
+        if !matches!(self.acs_type, AcsType::GenieAcs) {
+            return Err(Box::from("Unknown ACS type"));
+        }
+
+        let client = Client::new();
+
+        // Define the URL
+        let url = self.addr.clone() + "/files/" + &name;
+
+        // Create set of headers
+        let mut headers = HeaderMap::new();
+        headers.insert("fileType", HeaderValue::from_str(file_type)?);
+        headers.insert("oui", HeaderValue::from_str(oui)?);
+        headers.insert("productClass", HeaderValue::from_str(product_class)?);
+        headers.insert("version", HeaderValue::from_str(version)?);
+
+        // Read file
+        let file_bytes = std::fs::read(path)?;
+
+        // Send request
+        let response = client
+            .put(&url)
+            .headers(headers)
+            .body(file_bytes)
+            .send()?;
 
         if response.status().is_success() {
             return Ok(());
